66467647f0daf514535c8af66ba92ea10bbb05f4
Matheus Sousa de Jesus <matheuss.jesus2017@gmail.com>
2020-02-18 21:48:43 -0300

    atualizando o projeto

--- js/controller/ControllerPomodoro.js
+++ js/controller/ControllerPomodoro.js
@@ -3,17 +3,29 @@ class ControllerPomodoro {
 		/*Creating a copy of Jquery*/
 		let $ = document.querySelector.bind(document);
 
-		this._fieldTime = $("#time");
+		this._fieldTimeMinutes = $("#time-minutes");
+		this._fieldTimeSeconds = $("#time-seconds");
 	}
 	_startTime(){
-			let time = this._fieldTime.innerHTML;
-			console.log(this._fieldTime.innerHTML);
-			let stopWatch = setInterval(function(){
-			 time--;
+		let timeSeconds = this._fieldTimeSeconds.innerHTML;
+		let timeMinutes = this._fieldTimeMinutes.innerHTML;
+		console.log(timeMinutes);
+		let stopWatch = setInterval(function(){
+
+			if (timeSeconds < 1) {
+				timeMinutes--;
+				document.querySelector("#time-minutes").innerHTML = timeMinutes;
+			}	
+
+			if (timeSeconds < 1) {
+				timeSeconds = 59;
+			}
+			timeSeconds--;
+			if (timeSeconds >= 0) {
+				document.querySelector("#time-seconds").innerHTML = timeSeconds;
+			}
 			
-			document.querySelector("#time").innerHTML = time;
-			console.log(time);
-			if (time < 1) {
+			if (timeMinutes == 0 && timeSeconds < 1 ) {
 				clearInterval(stopWatch);
 			}
 		},1000);
