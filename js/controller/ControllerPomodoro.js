da60ca18c6c8577c12c82723ce57df2dd6199c7a
Matheus Sousa de Jesus <matheuss.jesus2017@gmail.com>
2020-02-19 16:33:53 -0300

    adicionando ações nos buttons

--- js/controller/ControllerPomodoro.js
+++ js/controller/ControllerPomodoro.js
@@ -2,21 +2,28 @@ class ControllerPomodoro {
 	constructor(){
 		/*Creating a copy of Jquery*/
 		let $ = document.querySelector.bind(document);
-
 		this._fieldTimeMinutes = $("#time-minutes");
 		this._fieldTimeSeconds = $("#time-seconds");
+		this._id = null;
+		this._button = document.querySelector(".btn-warning:nth-of-type(1)");
 	}
-	_startTime(){
+
+
+	startTime(){
 		let timeSeconds = this._fieldTimeSeconds.innerHTML;
 		let timeMinutes = this._fieldTimeMinutes.innerHTML;
-		console.log(timeMinutes);
-		let stopWatch = setInterval(function(){
-
+		
+		this._button.removeAttribute("id","start");
+		this._button.removeAttribute("onclick","controllerPomodoro.startTime()");
+		this._button.setAttribute("onclick","controllerPomodoro.pauseButton()");
+		this._button.setAttribute("id","pause");
+		this._button.textContent = "Pause";
+		this._id = setInterval(function(){
+			
 			if (timeSeconds < 1) {
 				timeMinutes--;
 				document.querySelector("#time-minutes").innerHTML = timeMinutes;
-			}	
-
+			}
 			if (timeSeconds < 1) {
 				timeSeconds = 59;
 			}
@@ -24,10 +31,26 @@ class ControllerPomodoro {
 			if (timeSeconds >= 0) {
 				document.querySelector("#time-seconds").innerHTML = timeSeconds;
 			}
-			
 			if (timeMinutes == 0 && timeSeconds < 1 ) {
 				clearInterval(stopWatch);
 			}
+
 		},1000);
 	}
+	pauseButton(){
+		this._button.removeAttribute("id","pause");
+		this._button.removeAttribute("onclick","controllerPomodoro.pauseButton()");
+		this._button.setAttribute("onclick","controllerPomodoro.startTime()");
+		this._button.setAttribute("id","pause");
+		let buttonPause = document.querySelector("#pause");
+		this._button.textContent = "Iniciar";
+		clearInterval(this._id);
+			
+	}
+	restart(){
+		this.pauseButton();
+		this._fieldTimeMinutes.textContent = "5";
+		this._fieldTimeSeconds.textContent = "00";
+	}
+
 }
