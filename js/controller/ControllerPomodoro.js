14c40777153a9bdfbda035d0720e88399829462d
Matheus Sousa de Jesus <matheuss.jesus2017@gmail.com>
2020-02-20 14:17:50 -0300

    adicionando formulario

--- js/controller/ControllerPomodoro.js
+++ js/controller/ControllerPomodoro.js
@@ -4,7 +4,9 @@ class ControllerPomodoro {
 		let $ = document.querySelector.bind(document);
 		this._fieldTimeMinutes = $("#time-minutes");
 		this._fieldTimeSeconds = $("#time-seconds");
+		this._timeForm = document.querySelector("#tempoForm");
 		this._id = null;
+		this._time = this.time();
 		this._button = document.querySelector(".btn-warning:nth-of-type(1)");
 	}
 
@@ -25,14 +27,14 @@ class ControllerPomodoro {
 				document.querySelector("#time-minutes").innerHTML = timeMinutes;
 			}
 			if (timeSeconds < 1) {
-				timeSeconds = 59;
+				timeSeconds = 60;
 			}
 			timeSeconds--;
 			if (timeSeconds >= 0) {
 				document.querySelector("#time-seconds").innerHTML = timeSeconds;
 			}
 			if (timeMinutes == 0 && timeSeconds < 1 ) {
-				clearInterval(stopWatch);
+				clearInterval(this._id);
 			}
 
 		},1000);
@@ -49,8 +51,25 @@ class ControllerPomodoro {
 	}
 	restart(){
 		this.pauseButton();
-		this._fieldTimeMinutes.textContent = "5";
-		this._fieldTimeSeconds.textContent = "00";
+		this._fieldTimeMinutes.textContent = this._time[0];
+		this._fieldTimeSeconds.textContent = this._time[1];
+	}
+	form(event){
+		event.preventDefault();
+		this._time = this._timeForm.value.split(':');
+		this._fieldTimeMinutes.innerHTML = this._time[0];
+		this._fieldTimeSeconds.innerHTML = this._time[1];
+		console.log(this._time);
+		this.zeraForm();
+	}
+	zeraForm(){
+		 this._timeForm.value = "00:00";
+	}
+	time(){
+		let time = [];
+		time[0] = this._fieldTimeMinutes.innerHTML;
+		time[1] = this._fieldTimeSeconds.innerHTML;
+		return time;
 	}
 
 }
