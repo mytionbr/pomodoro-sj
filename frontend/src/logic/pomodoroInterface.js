68ae8b81337d066ec9eb82c9a43696b2e82df910
mytionBr <matheuss.jesus2017@gmail.com>
2021-03-04 21:21:16 -0300

    criando um algoritmo para a contagem de tempo do pomodoro

--- frontend/src/logic/pomodoroInterface.js
+++ frontend/src/logic/pomodoroInterface.js
@@ -7,11 +7,21 @@ const pomodoroInterface  = ()=>{
 
 
     btnStart.addEventListener('click',()=>{
-        setTimeout(()=>{
-            let timePomodoro = time.textContent
+        let timePomodoro = time.textContent
             timePomodoro = timePomodoro.split(':')
-            time.innerHTML = `${timePomodoro[0] - 1 } : ${timePomodoro[1] - 1}`
-        },1000)
+
+        let minute = timePomodoro[0]
+        let second = timePomodoro[1]
+        
+        let i = setInterval(()=>{
+
+                    (minute == 0) ? ((second == 0) ? clearInterval(i) : second-- ) 
+                                  : (second == 0) ? (minute--,second='59') : second--
+                                  
+                    time.innerHTML = `${(minute < 10)? "0"+minute : minute} : ${(second < 10)? "0"+second : second}` 
+                        
+                },10)
+        
     })
 }
 
