6cc304314a6628850ae6217255553d330f7d4746
mytionBr <matheuss.jesus2017@gmail.com>
2021-04-22 00:30:38 -0300

    adicionando os icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCogs,faTasks,faUser } from '@fortawesome/free-solid-svg-icons'
 function App() {
   return (
    <div className="body">
@@ -10,9 +11,9 @@ function App() {
         </div>
         <nav className="links">
           <ul>
-            <li><a href="/#/configuracoes/cronometro"> <i className="fas fa-cogs"></i> Configurações</a></li>
-            <li><a href="/#/tarefas"> <i className="fas fa-tasks"></i> Tarefas</a></li>
-            <li><a href="/#/usuario"><i className="fas fa-user"></i> Usuário</a></li>
+            <li><a href="/#/configuracoes/cronometro"> <i><FontAwesomeIcon icon={faCogs} /></i> Configurações</a></li>
+            <li><a href="/#/tarefas"> <i><FontAwesomeIcon icon={faTasks} /></i> Tarefas</a></li>
+            <li><a href="/#/usuario"><i><FontAwesomeIcon icon={faUser} /></i>  Usuário</a></li>
           </ul>
         </nav>
       </header>
