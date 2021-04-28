c2fadde1be6902675bfe787171f2c88c54358bf0
mytionBr <matheuss.jesus2017@gmail.com>
2021-04-28 16:12:02 -0300

    configurando indexeddb

---
+++ client/src/indexeddb-common.js
@@ -0,0 +1,6 @@
+const  indexedDB = (window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB,
+IDBTransaction  = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction,
+baseName 	    = "baseName",
+storeName 	    = "storeName")
+
+export default indexedDB
