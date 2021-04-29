8b040032b8b7cffdc2d79d3df0b2bcbc7c0a604c
mytionBr <matheuss.jesus2017@gmail.com>
2021-04-29 16:35:29 -0300

    adicionando efeito de selecionando

--- client/src/screens/SettingsScreen.js
+++ client/src/screens/SettingsScreen.js
@@ -10,33 +10,58 @@ const SettingsScreen = (props) => {
     const initialSetting = "cronometro"
     
     const [currentSetting,setCurrentSetting] = useState(initialSetting)
+    
     let btnsOptions = {
             option_chronometer: '',
             option_aplication: '',
             option_notifications: '',
+
     }
 
     useEffect(()=>{
        setCurrentSetting(props.match.params.setting)
 
-       btnsOptions.option_chronometer = document.querySelector("#option_chronometer")
-       btnsOptions.option_aplication = document.querySelector("#option_aplication")
-       btnsOptions.option_notifications = document.querySelector("#option_motifications")
+       popularObject()
+       removeSelectorStyle()
+       addSelectionStyle()
     })
+
+    const popularObject = ()=>{
+        btnsOptions.option_chronometer = document.querySelector("#option_chronometer")
+        btnsOptions.option_aplication = document.querySelector("#option_aplication")
+        btnsOptions.option_notifications = document.querySelector("#option_motifications")
+        
+    }
 
     const removeSelectorStyle = ()=>{
        for (let prop in btnsOptions) {
-              console.log(btnsOptions[prop])
-              btnsOptions[prop].classList.remove('menu-section-selector')
+            btnsOptions[prop].classList.remove('menu-section-selector')
         }
     }
-    
-    const handleSelectOption = (e)=>{
-        removeSelectorStyle()
-        e.target.parentNode.classList.add('menu-section-selector')
+
+    const addSelectionStyle = ()=>{
+        console.log(btnsOptions.option_aplication)
+        switch(currentSetting){
+            case 'cronometro':
+                btnsOptions.option_chronometer.classList.add('menu-section-selector') 
+                break
+            case 'aplicacao':
+                btnsOptions.option_aplication.classList.add('menu-section-selector') 
+                break
+            case 'notificacao':
+                btnsOptions.option_notifications.classList.add('menu-section-selector')
+                break
+            default:
+                btnsOptions.option_chronometer.classList.add('menu-section-selector') 
+                break
+        }
+
     }
 
+
+
     const getSetting = current =>{
+        
         switch(current){
             case 'cronometro':
                 return <ChronometerSetting/>
@@ -53,13 +78,14 @@ const SettingsScreen = (props) => {
     return (
         <div className="container-config">
         <div className="menu-config">
-            <div onClick={handleSelectOption} className="menu-section" id="option_chronometer"><Link to="/configuracoes/cronometro"> <i className="fas fa-stopwatch"></i> Cronômetro</Link></div>
-            <div onClick={handleSelectOption} className="menu-section" id="option_aplication"><Link to="/configuracoes/aplicacao"><i className="fas fa-desktop"></i> Aplicação</Link></div>
-            <div onClick={handleSelectOption} className="menu-section" id="option_motifications"><Link to="/configuracoes/notificacao"><i className="far fa-bell"></i> Notificações</Link></div>
+            <div className="menu-section" id="option_chronometer"><Link to="/configuracoes/cronometro"> <i className="fas fa-stopwatch"></i> Cronômetro</Link></div>
+            <div className="menu-section" id="option_aplication"><Link to="/configuracoes/aplicacao"><i className="fas fa-desktop"></i> Aplicação</Link></div>
+            <div className="menu-section" id="option_motifications"><Link to="/configuracoes/notificacao"><i className="far fa-bell"></i> Notificações</Link></div>
         </div>
        
-        <div id="setting_main"></div>
-            {getSetting(currentSetting)}
+            <div id="setting_main">
+                {getSetting(currentSetting)}
+            </div>
         </div>
     )
 }
