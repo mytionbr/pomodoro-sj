7d2cd2d8024d319f4cc7d567a32470e1e02ba1b4
mytionBr <matheuss.jesus2017@gmail.com>
2021-04-30 11:27:36 -0300

    refatorando e corrigindo erro de seleçao de btn

--- client/src/screens/SettingsScreen.js
+++ client/src/screens/SettingsScreen.js
@@ -17,54 +17,11 @@ const SettingsScreen = (props) => {
 
   const [currentSetting, setCurrentSetting] = useState(initialSetting);
 
-  let btnsOptions = {
-    option_chronometer: "",
-    option_aplication: "",
-    option_notifications: "",
-  };
-
   useEffect(() => {
     setCurrentSetting(props.match.params.setting);
-
-    popularObject();
-    removeSelectorStyle();
-    addSelectionStyle();
   });
 
-  const popularObject = () => {
-    btnsOptions.option_chronometer = document.querySelector(
-      "#option_chronometer"
-    );
-    btnsOptions.option_aplication = document.querySelector(
-      "#option_aplication"
-    );
-    btnsOptions.option_notifications = document.querySelector(
-      "#option_motifications"
-    );
-  };
-
-  const removeSelectorStyle = () => {
-    for (let prop in btnsOptions) {
-      btnsOptions[prop].classList.remove("menu-section-selector");
-    }
-  };
-
-  const addSelectionStyle = () => {
-    console.log(btnsOptions.option_aplication);
-    switch (currentSetting) {
-      case "cronometro":
-        btnsOptions.option_chronometer.classList.add("menu-section-selector");
-        break;
-      case "aplicacao":
-        btnsOptions.option_aplication.classList.add("menu-section-selector");
-        break;
-      case "notificacao":
-        btnsOptions.option_notifications.classList.add("menu-section-selector");
-        break;
-      default:
-        btnsOptions.option_chronometer.classList.add("menu-section-selector");
-    }
-  };
+  
 
   const getSetting = (current) => {
     switch (current) {
@@ -82,7 +39,11 @@ const SettingsScreen = (props) => {
   return (
     <div className="container-config">
       <div className="menu-config">
-        <div className="menu-section" id="option_chronometer">
+        <div className={
+          currentSetting === 'cronometro' 
+          ?'menu-section menu-section-selector' 
+          :'menu-section'  
+          } id="option_chronometer">
           <Link to="/configuracoes/cronometro">
             {" "}
             <i>
@@ -91,7 +52,11 @@ const SettingsScreen = (props) => {
             Cronômetro
           </Link>
         </div>
-        <div className="menu-section" id="option_aplication">
+        <div className={
+          currentSetting === 'aplicacao' 
+          ?'menu-section menu-section-selector' 
+          :'menu-section'  
+          } id="option_aplication">
           <Link to="/configuracoes/aplicacao">
             <i>
               <FontAwesomeIcon icon={faDesktop} />
@@ -99,7 +64,11 @@ const SettingsScreen = (props) => {
             Aplicação
           </Link>
         </div>
-        <div className="menu-section" id="option_motifications">
+        <div className={
+          currentSetting === 'notificacao' 
+          ?'menu-section menu-section-selector' 
+          :'menu-section'  
+          } id="option_motifications">
           <Link to="/configuracoes/notificacao">
             <i>
               <FontAwesomeIcon icon={faBell} />
