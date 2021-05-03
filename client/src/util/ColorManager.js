358f4ef46a861b08ca8690f56a315de9b91620b3
mytionBr <matheuss.jesus2017@gmail.com>
2021-05-03 15:23:17 -0300

    adicionando função que escurece as cores hexadecimais

--- client/src/util/ColorManager.js
+++ client/src/util/ColorManager.js
@@ -35,7 +35,7 @@ export default class  ColorManager{
                         ...this.settings.pomodoro,
                         background:{
                                 primary:color,
-                                secondary:`lighten(${color},50%)`                           }}
+                                secondary:this.LightenDarkenColor(color,-15)                         }}
                     }) 
                     console.log(this.settings)
                 break;
@@ -46,7 +46,7 @@ export default class  ColorManager{
                         ...this.settings.shortBreak,
                         background:{
                                 primary:color,
-                                secondary:`lighten(${color},50%)`                           }}
+                                secondary:this.LightenDarkenColor(color,-15)                          }}
                     }) 
                 break;
             case 'longBreak':
@@ -56,11 +56,43 @@ export default class  ColorManager{
                         ...this.settings.longBreak,
                         background:{
                                 primary:color,
-                                secondary:`lighten(${color},50%)`                           }}
+                                secondary:this.LightenDarkenColor(color,-15)                           }}
                     }) 
+                    
                 break;
             default:
                 break;
         }
+    }
+
+    LightenDarkenColor(col, amt) {
+  
+        let usePound = false;
+      
+        if (col[0] === "#") {
+            col = col.slice(1);
+            usePound = true;
+        }
+     
+        let num = parseInt(col,16);
+     
+        let r = (num >> 16) + amt;
+     
+        if (r > 255) r = 255;
+        else if  (r < 0) r = 0;
+     
+        let b = ((num >> 8) & 0x00FF) + amt;
+     
+        if (b > 255) b = 255;
+        else if  (b < 0) b = 0;
+     
+        let g = (num & 0x0000FF) + amt;
+     
+        if (g > 255) g = 255;
+        else if (g < 0) g = 0;
+     
+        return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+         
+         
     }
 }
