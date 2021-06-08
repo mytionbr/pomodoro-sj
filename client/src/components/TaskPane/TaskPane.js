f19c6989ed7b683ae6cedb489b86286dedc47620
mytionBr <matheuss.jesus2017@gmail.com>
2021-06-08 10:14:26 -0300

    evoluindo o painel de tarefas

--- client/src/components/TaskPane/TaskPane.js
+++ client/src/components/TaskPane/TaskPane.js
@@ -4,6 +4,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus} from "@fortawesome/free-solid-svg-icons";
 function TaskPane() {
     
+    const tasks = [
+        {
+            taskId:'1',
+            categoryField: "Faculdade",
+            descriptionField:"Trabalho de contabilidade",
+            sessionField:"4",
+            doneField:false
+        },
+        {
+            taskId:'2',
+            categoryField: "Faculdade",
+            descriptionField:"Trabalho de contabilidade",
+            sessionField:"4",
+            doneField:false
+        },
+    ] 
+
     const CategoryInput = ()=>{
         return (
                 <input type="text" className="category_input" name="category_input" placeholder="Categoria"></input>
@@ -12,7 +29,7 @@ function TaskPane() {
 
     const TaskInput = ()=>{
         return (
-                <input type="text" className="task_input" name="task_input" placeholder="Escreva uma tarefa"></input>
+                <input type="text" className="description_input" name="description_input" placeholder="Escreva uma tarefa"></input>
         )
     }
 
@@ -35,10 +52,51 @@ function TaskPane() {
             </div>
         )
     }
+
+    const Task =(props)=>{
+        return <li className="task_item" key={props.id}>
+            <input 
+                type="text" 
+                className="task_item_category" 
+                value={props.category} />
+
+            <input 
+                type="text" 
+                className="task_item_description" 
+                value={props.description} />
+            
+            <input 
+                type="number" 
+                className="task_item_session" 
+                value={props.session} />
+            
+            <input 
+                type="button" 
+                className="task_item_done" 
+                value={props.done ? "V" : ''} />
+        </li>
+    }
+
+    const TaskList = ({tasks})=>{
+        return (
+            <ul className="task_list">
+                {tasks.map(task => {
+                    return <Task 
+                                id={task.id}
+                                category={task.categoryField} 
+                                description={task.descriptionField}
+                                session={task.sessionField}
+                                done={task.doneField}/>
+                    
+                })}
+            </ul>
+        )
+    }
     
     return (
-        <div>
+        <div className="task_container">
             <TaskHeaderContainer/>
+            <TaskList tasks={tasks}/>
         </div>
     )
 }
